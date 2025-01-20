693406a93fb94fb39603ab6a6dc363a06ddae962
DaffaPutra7 <putradika3604@gmail.com>
2025-01-20 22:33:15 +0700

    Menambahkan file MerkRepository pada package repository

---
+++ app/src/main/java/com/example/a3_133/repository/MerkRepository.kt
@@ -0,0 +1,43 @@
+package com.example.a3_133.repository
+
+import com.example.a3_133.model.Merk
+import com.example.a3_133.service.MerkService
+import java.io.IOException
+
+interface MerkRepository {
+    suspend fun getMerk(): List<Merk>
+    suspend fun getMerkById(idmerk : String) : Merk
+    suspend fun insertMerk(merk: Merk)
+    suspend fun updateMerk(idmerk: String, merk: Merk)
+    suspend fun deleteMerk(idmerk: String)
+}
+
+class NetworkMerkRepository( private val merkApiService: MerkService) : MerkRepository {
+    override suspend fun insertMerk(merk: Merk) {
+        merkApiService.insertMerk(merk)
+    }
+
+    override suspend fun updateMerk(idmerk: String, merk: Merk) {
+        merkApiService.updateMerk(idmerk, merk)
+    }
+
+    override suspend fun deleteMerk(idmerk: String) {
+        try {
+            val response = merkApiService.deleteMerk(idmerk)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete merk. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMerk(): List<Merk> = merkApiService.getMerk()
+
+    override suspend fun getMerkById(idmerk: String): Merk {
+        return merkApiService.getMerkById(idmerk)
+    }
+}
