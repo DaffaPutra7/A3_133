1d58d8d5e639617819e189ef8a57de1f8136c73d
DaffaPutra7 <putradika3604@gmail.com>
2025-01-21 19:04:43 +0700

    Membuat file UpdateMerkView di package view dan mengubah nama fun pada file InsertMerkView.kt

--- app/src/main/java/com/example/a3_133/ui/view/UpdateMerkView.kt
+++ app/src/main/java/com/example/a3_133/ui/view/UpdateMerkView.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -16,51 +14,52 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a3_133.ui.customwidget.CustomeTopAppBar
-import com.example.a3_133.ui.viewmodel.InsertMerkViewModel
-import com.example.a3_133.ui.viewmodel.InsertUiEvent
-import com.example.a3_133.ui.viewmodel.InsertUiState
 import com.example.a3_133.ui.viewmodel.PenyediaViewModel
+import com.example.a3_133.ui.viewmodel.UpdateMerkViewModel
+import com.example.a3_133.ui.viewmodel.UpdateUiEvent
+import com.example.a3_133.ui.viewmodel.UpdateUiState
 import kotlinx.coroutines.launch
-
-object DestinasiEntry : DestinasiNavigasi{
-    override val route = "item_entry"
-    override val titleRes = "Entry Mhs"
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EntryMerkScreen(
+fun UpdateMerkView(
     navigateBack: () -> Unit,
+    id: String,
     modifier: Modifier = Modifier,
-    viewModel: InsertMerkViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+    viewModel: UpdateMerkViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(id) {
+        viewModel.getMerkById(id)
+    }
 
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             CustomeTopAppBar(
-                title = DestinasiEntry.titleRes,
+                title = "Update Merk",
                 canNavigateBack = true,
                 scrollBehavior = scrollBehavior,
                 navigateUp = navigateBack
             )
         }
     ) { innerPadding ->
-        EntryBodyMerk(
-            insertUiState = viewModel.uiState,
-            onMerkValueChange = viewModel::updateInsertMerkState,
+        UpdateBodyMerk(
+            updateUiState = uiState,
+            onMerkValueChange = viewModel::updateMerkState,
             onSaveClick = {
                 coroutineScope.launch {
-                    viewModel.insertMerk()
+                    viewModel.updateMerk()
                     navigateBack()
                 }
             },
@@ -73,18 +72,18 @@ fun EntryMerkScreen(
 }
 
 @Composable
-fun EntryBodyMerk(
-    insertUiState: InsertUiState,
-    onMerkValueChange: (InsertUiEvent) -> Unit,
+fun UpdateBodyMerk(
+    updateUiState: UpdateUiState,
+    onMerkValueChange: (UpdateUiEvent) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
-){
+) {
     Column(
         verticalArrangement = Arrangement.spacedBy(18.dp),
         modifier = modifier.padding(12.dp)
     ) {
-        FormInputMerk(
-            insertUiEvent = insertUiState.insertUiEvent,
+        FormUpdateMerk(
+            updateUiEvent = updateUiState.updateUiEvent,
             onValueChange = onMerkValueChange,
             modifier = Modifier.fillMaxWidth()
         )
@@ -93,57 +92,47 @@ fun EntryBodyMerk(
             shape = MaterialTheme.shapes.small,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Simpan")
+            Text(text = "Update")
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormInputMerk(
-    insertUiEvent: InsertUiEvent,
+fun FormUpdateMerk(
+    updateUiEvent: UpdateUiEvent,
     modifier: Modifier = Modifier,
-    onValueChange: (InsertUiEvent) -> Unit = {},
+    onValueChange: (UpdateUiEvent) -> Unit = {},
     enabled: Boolean = true
-){
+) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         OutlinedTextField(
-            value = insertUiEvent.idMerk,
-            onValueChange = { onValueChange(insertUiEvent.copy(idMerk = it))},
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            value = updateUiEvent.idMerk,
+            onValueChange = { onValueChange(updateUiEvent.copy(idMerk = it)) },
             label = { Text("ID Merk") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
         OutlinedTextField(
-            value = insertUiEvent.namaMerk,
-            onValueChange = { onValueChange(insertUiEvent.copy(namaMerk = it))},
+            value = updateUiEvent.namaMerk,
+            onValueChange = { onValueChange(updateUiEvent.copy(namaMerk = it)) },
             label = { Text("Nama Merk") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
+
         OutlinedTextField(
-            value = insertUiEvent.deskripsiMerk,
-            onValueChange = { onValueChange(insertUiEvent.copy(deskripsiMerk = it))},
+            value = updateUiEvent.deskripsiMerk,
+            onValueChange = { onValueChange(updateUiEvent.copy(deskripsiMerk = it)) },
             label = { Text("Deskripsi Merk") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
-        )
-        if (enabled) {
-            Text(
-                text = "Isi Smeua Data!",
-                modifier = Modifier.padding(12.dp)
-            )
-        }
-        Divider(
-            thickness = 8.dp,
-            modifier = Modifier.padding(12.dp)
         )
     }
 }
