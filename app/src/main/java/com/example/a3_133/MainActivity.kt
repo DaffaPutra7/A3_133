2a8d97242626eda2daf70a7751557804d077fc72
DaffaPutra7 <putradika3604@gmail.com>
2025-01-21 19:28:06 +0700

    Melakukan perubahan gradle, membuat file A3App di package viewmodel yang akan dipanggil di file MainActivity.kt

--- app/src/main/java/com/example/a3_133/MainActivity.kt
+++ app/src/main/java/com/example/a3_133/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a3_133.ui.theme.A3_133Theme
+import com.example.a3_133.ui.viewmodel.A3App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             A3_133Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    A3App(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
