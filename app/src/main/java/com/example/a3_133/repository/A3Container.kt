8f6e449115ac08c03f25104bc309791ccc5b74d0
DaffaPutra7 <putradika3604@gmail.com>
2025-01-22 20:04:43 +0700

    Memanggil PemasokService dan PemasokRepository di A3Container.kt

--- app/src/main/java/com/example/a3_133/repository/A3Container.kt
+++ app/src/main/java/com/example/a3_133/repository/A3Container.kt
@@ -1,6 +1,7 @@
 package com.example.a3_133.repository
 
 import com.example.a3_133.service.MerkService
+import com.example.a3_133.service.PemasokService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -8,6 +9,7 @@ import retrofit2.Retrofit
 
 interface AppContainer{
     val merkRepository: MerkRepository
+    val pemasokRepository: PemasokRepository
 }
 
 class A3Container : AppContainer {
@@ -19,4 +21,7 @@ class A3Container : AppContainer {
 
     private val merkService: MerkService by lazy { retrofit.create(MerkService::class.java) }
     override val merkRepository: MerkRepository by lazy { NetworkMerkRepository(merkService) }
+
+    private val pemasokService: PemasokService by lazy { retrofit.create(PemasokService::class.java) }
+    override val pemasokRepository: PemasokRepository by lazy { NetworkPemasokRepository(pemasokService) }
 }
