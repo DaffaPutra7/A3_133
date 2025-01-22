556ad429e99fbc5ad5d524900c7e5cef1c046c84
DaffaPutra7 <putradika3604@gmail.com>
2025-01-22 19:39:31 +0700

    Membuat file Pemasok

---
+++ app/src/main/java/com/example/a3_133/model/Pemasok.kt
@@ -0,0 +1,20 @@
+package com.example.a3_133.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class Pemasok(
+    @SerialName("id_pemasok")
+    val idPemasok: String,
+
+    @SerialName("nama_pemasok")
+    val namaPemasok: String,
+
+    @SerialName("alamat_pemasok")
+    val alamatPemasok: String,
+
+    @SerialName("telepon_pemasok")
+    val teleponPemasok: String
+)
