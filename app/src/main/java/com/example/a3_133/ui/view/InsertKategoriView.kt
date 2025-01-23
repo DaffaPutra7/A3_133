712fecdaa59189d59891652ab18801fa7e2e8e3c
DaffaPutra7 <putradika3604@gmail.com>
2025-01-23 18:04:12 +0700

    Membuat file InsertKategoriView

---
+++ app/src/main/java/com/example/a3_133/ui/view/InsertKategoriView.kt
@@ -0,0 +1,139 @@
+package com.example.a3_133.ui.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a3_133.ui.customwidget.CustomeTopAppBar
+import com.example.a3_133.ui.navigation.DestinasiNavigasi
+import com.example.a3_133.ui.viewmodel.InsertKategoriUiEvent
+import com.example.a3_133.ui.viewmodel.InsertKategoriUiState
+import com.example.a3_133.ui.viewmodel.InsertKategoriViewModel
+import com.example.a3_133.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEntryKategori : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Kategori"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKategoriScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertKategoriViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBar(
+                title = DestinasiEntryKategori.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBodyKategori(
+            insertKategoriUiState = viewModel.kategoriuiState,
+            onKategoriValueChange = viewModel::updateInsertKategoriState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertKategori()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBodyKategori(
+    insertKategoriUiState: InsertKategoriUiState,
+    onKategoriValueChange: (InsertKategoriUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputKategori(
+            insertKategoriUiEvent = insertKategoriUiState.insertKategoriUiEvent,
+            onValueChange = onKategoriValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputKategori(
+    insertKategoriUiEvent: InsertKategoriUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertKategoriUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertKategoriUiEvent.namaKategori,
+            onValueChange = { onValueChange(insertKategoriUiEvent.copy(namaKategori = it))},
+            label = { Text("Nama Kategori") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertKategoriUiEvent.deskripsiKategori,
+            onValueChange = { onValueChange(insertKategoriUiEvent.copy(deskripsiKategori = it))},
+            label = { Text("Deskripsi Kategori") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi Smeua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
