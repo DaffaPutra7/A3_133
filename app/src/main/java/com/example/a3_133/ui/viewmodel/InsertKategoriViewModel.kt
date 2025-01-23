0c94739c55601fb0c0e9fd47e0e30c3c3f37a0aa
DaffaPutra7 <putradika3604@gmail.com>
2025-01-23 17:05:26 +0700

    Membuat file InsertKategoriViewModel

---
+++ app/src/main/java/com/example/a3_133/ui/viewmodel/InsertKategoriViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.a3_133.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a3_133.model.Kategori
+import com.example.a3_133.repository.KategoriRepository
+import kotlinx.coroutines.launch
+
+class InsertKategoriViewModel(private val kategori: KategoriRepository) : ViewModel() {
+
+    var kategoriuiState by mutableStateOf(InsertKategoriUiState())
+        private set
+
+    fun updateInsertKategoriState(insertkategoriUiEvent: InsertKategoriUiEvent){
+        kategoriuiState = InsertKategoriUiState(insertKategoriUiEvent = insertkategoriUiEvent)
+    }
+
+    suspend fun insertKategori(){
+        viewModelScope.launch {
+            try {
+                kategori.insertKategori(kategoriuiState.insertKategoriUiEvent.toKategori())
+            } catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertKategoriUiState(
+    val insertKategoriUiEvent: InsertKategoriUiEvent = InsertKategoriUiEvent()
+)
+
+data class InsertKategoriUiEvent(
+    val idKategori: String = "",
+    val namaKategori: String = "",
+    val deskripsiKategori: String = ""
+)
+
+fun InsertKategoriUiEvent.toKategori(): Kategori = Kategori(
+    idKategori = idKategori,
+    namaKategori = namaKategori,
+    deskripsiKategori = deskripsiKategori
+)
+
+fun Kategori.toUiStateKategori(): InsertKategoriUiState = InsertKategoriUiState(
+    insertKategoriUiEvent = toInsertKategoriUiEvent()
+)
+
+fun Kategori.toInsertKategoriUiEvent(): InsertKategoriUiEvent = InsertKategoriUiEvent(
+    idKategori = idKategori,
+    namaKategori = namaKategori,
+    deskripsiKategori = deskripsiKategori
+)
