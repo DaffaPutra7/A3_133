e0d101b9db34757f88f5fd692559d5c77c8ff826
DaffaPutra7 <putradika3604@gmail.com>
2025-01-26 09:35:49 +0700

    Melakukan perubahan pada file PengelolaHalaman.kt

--- app/src/main/java/com/example/a3_133/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/a3_133/ui/navigation/PengelolaHalaman.kt
@@ -8,6 +8,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.a3_133.ui.home.DestinasiHomeScreen
+import com.example.a3_133.ui.home.HomeScreen
 import com.example.a3_133.ui.view.merk.DestinasiEntry
 import com.example.a3_133.ui.view.kategori.DestinasiEntryKategori
 import com.example.a3_133.ui.view.pemasok.DestinasiEntryPemasok
@@ -23,22 +25,44 @@ import com.example.a3_133.ui.view.pemasok.PemasokHomeScreen
 import com.example.a3_133.ui.view.kategori.UpdateKategoriView
 import com.example.a3_133.ui.view.merk.UpdateMerkView
 import com.example.a3_133.ui.view.pemasok.UpdatePemasokView
+import com.example.a3_133.ui.view.produk.DestinasiDetailProduk
+import com.example.a3_133.ui.view.produk.DestinasiEntryProduk
+import com.example.a3_133.ui.view.produk.DestinasiHomeProduk
+import com.example.a3_133.ui.view.produk.DetailProdukView
+import com.example.a3_133.ui.view.produk.EntryProdukScreen
+import com.example.a3_133.ui.view.produk.HomeProdukScreen
+import com.example.a3_133.ui.view.produk.UpdateProdukView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
 
     NavHost(
         navController = navController,
-        startDestination = DestinasiHomeKategori.route,
+        startDestination = DestinasiHomeScreen.route,
         modifier = Modifier
     ) {
+        // Home
+        composable(DestinasiHomeScreen.route) {
+            HomeScreen(
+                onHalamanStart = {
+                    navController.navigate(DestinasiHomeProduk.route)
+                },
+                modifier = Modifier
+            )
+        }
 
+        // Merk
         composable(DestinasiHome.route) {
             MerkHomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
                 navigateToUpdate = { id ->
                     navController.navigate("update_merk/$id") // Navigasi ke update
-                }
+                },
+                navigateBack = {navController.navigate(DestinasiHomeProduk.route) {
+                    popUpTo(DestinasiHomeProduk.route) {
+                        inclusive = true
+                    }
+                } }
             )
         }
 
@@ -65,12 +89,18 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             )
         }
 
+        // Pemasok
         composable(DestinasiHomePemasok.route) {
             PemasokHomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntryPemasok.route)},
                 navigateToUpdate = { id ->
                     navController.navigate("update_pemasok/$id") // Navigasi ke update
-                }
+                },
+                navigateBack = {navController.navigate(DestinasiHomeProduk.route) {
+                    popUpTo(DestinasiHomeProduk.route) {
+                        inclusive = true
+                    }
+                } }
             )
         }
 
@@ -97,12 +127,18 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             )
         }
 
+        // Kategori
         composable(DestinasiHomeKategori.route) {
             KategoriHomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntryKategori.route)},
                 navigateToUpdate = { id ->
                     navController.navigate("update_kategori/$id") // Navigasi ke update
-                }
+                },
+                navigateBack = {navController.navigate(DestinasiHomeProduk.route) {
+                    popUpTo(DestinasiHomeProduk.route) {
+                        inclusive = true
+                    }
+                } }
             )
         }
 
@@ -123,6 +159,74 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             val id = backStackEntry.arguments?.getString("id") ?: return@composable
             UpdateKategoriView(
                 id = id,
+                navigateBack = {
+                    navController.navigateUp()
+                }
+            )
+        }
+
+        // Produk
+        composable(DestinasiHomeProduk.route) {
+            HomeProdukScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntryProduk.route) },
+                onDetailClick = { id ->
+                    navController.navigate("${DestinasiDetailProduk.route}/$id")
+                },
+                navigateBack = {
+                    navController.navigate(DestinasiHomeScreen.route) {
+                        popUpTo(DestinasiHomeScreen.route) {
+                            inclusive = true
+                        }
+                    }
+                },
+                onProdukClick = {
+                    navController.navigate(DestinasiHomeProduk.route)
+                },
+                onPemasokClick = {
+                    navController.navigate(DestinasiHomePemasok.route)
+                },
+                onMerkClick = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+
+        composable(DestinasiEntryProduk.route) {
+            EntryProdukScreen(navigateBack = {
+                navController.navigate(DestinasiHomeProduk.route) {
+                    popUpTo(DestinasiHomeProduk.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+
+        composable(
+            route = "${DestinasiDetailProduk.route}/{id}",
+            arguments = listOf(navArgument(name = "id") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id") ?: return@composable
+            DetailProdukView(
+                idProduk = id,
+                navigateBack = {
+                    navController.navigateUp()
+                },
+                navigateToEdit = { idProduk ->
+                    navController.navigate("update_produk/$idProduk")
+                },
+                navigateToKategori = {
+                    navController.navigate(DestinasiHomeKategori.route)
+                }
+            )
+        }
+
+        composable(
+            route = "update_produk/{id}",
+            arguments = listOf(navArgument(name = "id") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id") ?: return@composable
+            UpdateProdukView(
+                idProduk = id,
                 navigateBack = {
                     navController.navigateUp()
                 }
