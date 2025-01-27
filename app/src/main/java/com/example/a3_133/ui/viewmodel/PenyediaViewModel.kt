44361a31f5928cfb92e0e6a47b5f02f00c72d8c7
DaffaPutra7 <putradika3604@gmail.com>
2025-01-27 09:05:41 +0700

    Membuat file DetailPemasokViewModel dan menambah initializer di file PenyediaViewModel.kt

--- app/src/main/java/com/example/a3_133/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a3_133/ui/viewmodel/PenyediaViewModel.kt
@@ -12,6 +12,7 @@ import com.example.a3_133.ui.viewmodel.kategori.UpdateKategoriViewModel
 import com.example.a3_133.ui.viewmodel.merk.HomeMerkViewModel
 import com.example.a3_133.ui.viewmodel.merk.InsertMerkViewModel
 import com.example.a3_133.ui.viewmodel.merk.UpdateMerkViewModel
+import com.example.a3_133.ui.viewmodel.pemasok.DetailPemasokViewModel
 import com.example.a3_133.ui.viewmodel.pemasok.HomePemasokViewModel
 import com.example.a3_133.ui.viewmodel.pemasok.InsertPemasokViewModel
 import com.example.a3_133.ui.viewmodel.pemasok.UpdatePemasokViewModel
@@ -29,6 +30,10 @@ object PenyediaViewModel {
         initializer { HomePemasokViewModel(aplikasiA3().container.pemasokRepository) }
         initializer { InsertPemasokViewModel(aplikasiA3().container.pemasokRepository) }
         initializer { UpdatePemasokViewModel(aplikasiA3().container.pemasokRepository) }
+        initializer {
+            val savedStateHandle = createSavedStateHandle()
+            DetailPemasokViewModel(savedStateHandle, aplikasiA3().container.pemasokRepository)
+        }
 
         initializer { HomeKategoriViewModel(aplikasiA3().container.kategoriRepository) }
         initializer { InsertKategoriViewModel(aplikasiA3().container.kategoriRepository) }
---
+++ app/src/main/java/com/example/a3_133/ui/viewmodel/pemasok/DetailPemasokViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.a3_133.ui.viewmodel.pemasok
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.a3_133.model.Pemasok
+import com.example.a3_133.repository.PemasokRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class DetailPemasokUiState {
+    data class Success(val pemasok: Pemasok) : DetailPemasokUiState()
+    data class Error(val message: String) : DetailPemasokUiState()
+    object Loading : DetailPemasokUiState()
+}
+
+class DetailPemasokViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val pemasokRepository: PemasokRepository
+) : ViewModel() {
+    private val _idPemasok: String = checkNotNull(savedStateHandle["id"])
+
+    private val _detailPemasokUiState = MutableStateFlow<DetailPemasokUiState>(DetailPemasokUiState.Loading)
+    val detailPemasokUiState: StateFlow<DetailPemasokUiState> = _detailPemasokUiState.asStateFlow()
+
+    var updateTrigger = MutableStateFlow(false)
+        private set
+
+    init {
+        getPemasokById(_idPemasok)
+    }
+
+    fun getPemasokById(idPemasok: String) {
+        viewModelScope.launch {
+            _detailPemasokUiState.value = DetailPemasokUiState.Loading
+            _detailPemasokUiState.value = try {
+                val pemasok = pemasokRepository.getPemasokById(idPemasok)
+                DetailPemasokUiState.Success(pemasok)
+            } catch (e: IOException) {
+                DetailPemasokUiState.Error("Terjadi kesalahan jaringan")
+            } catch (e: HttpException) {
+                DetailPemasokUiState.Error("Terjadi kesalahan server")
+            }
+        }
+    }
+}
